4c58100e235f63b7ec42769506c8339053c64694
martin Høva <mrhova46@gmail.com>
2023-08-12 21:28:42 +0200

    day 6 finished

---
+++ 21/day6/main.py
@@ -0,0 +1,45 @@
+def part1(inputfilename):
+    with open(inputfilename) as f:
+            #parse input and cast to ints
+        data = f.readline().split(",")
+        for i in range(len(data)):
+            data[i] = int(data[i])
+            
+    for i in range(80):
+        numberOfFish = len(data)
+        for j in range(numberOfFish):
+            if data[j] == 0:
+                data[j] = 6
+                data.append(8)
+            else:
+                data[j] = data[j] - 1
+    
+    return len(data)
+
+def part2(inputfilename):
+    
+    data = open(inputfilename).readline().split(",")
+    
+    fishCounts = [0] * 9
+    for fish in data:
+        fishCounts[int(fish)] += 1
+        
+    for _ in range(256):
+        resets = fishCounts.pop(0)
+        fishCounts.append(resets)
+        fishCounts[6] += resets
+        
+    return sum(fishCounts)
+     
+     
+def main(test = False):
+    if test:
+        filename = '21/day6/testinput.txt'
+    else:
+        filename = '21/day6/input.txt'
+        
+    print(f"part 1 : {part1(filename)}")
+    print(f"part 2 : {part2(filename)}")
+    
+    
+main()
