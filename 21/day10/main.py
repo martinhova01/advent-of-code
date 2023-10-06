1a352e78bcc59ddb6c09029e434063082a953792
martin Høva <mrhova46@gmail.com>
2023-10-06 11:28:55 +0200

    day 10 completed

---
+++ 21/day10/main.py
@@ -0,0 +1,66 @@
+from collections import deque
+validPairs = {"(": ")", "{": "}", "[": "]", "<": ">"}
+def part1(filename):
+    data = [x for x in open(filename).read().split("\n")]
+    incompleteLines = list(data)
+    
+    scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    
+    result = 0
+    for s in data:
+        stack = deque()
+
+        for i in range(len(s)):
+            c = s[i]
+            if c in validPairs:
+                stack.append(c)
+            else:
+                topElem = stack.pop()
+                if c != validPairs[topElem]:
+                    result += scores[c]
+                    incompleteLines.remove(s)
+                    break
+    return (result, incompleteLines)
+
+        
+
+
+def part2(filename):
+    
+    incompleteLines = part1(filename)[1]
+    scores = {")": 1, "]": 2, "}": 3, ">": 4}
+    
+    lineScores = []
+    for s in incompleteLines:
+        stack = deque()
+
+        for i in range(len(s)):
+            c = s[i]
+            if c in validPairs:
+                stack.append(c)
+            else:
+                stack.pop()
+                
+        completionString = ""
+        while stack:
+            completionString += validPairs[stack.pop()]
+            
+        score = 0
+        for c in completionString:
+            score = 5 * score + scores[c]
+        lineScores.append(score)
+        
+    lineScores.sort()
+    return lineScores[(len(lineScores) // 2)]
+            
+    
+def main(test = False):
+    if test:
+        filename = '21/day10/testinput.txt'
+    else:
+        filename = '21/day10/input.txt'
+        
+    print(f"part 1 : {part1(filename)[0]}")
+    print(f"part 2 : {part2(filename)}")
+    
+main()
