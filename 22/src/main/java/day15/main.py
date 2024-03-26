e09ec735dff7b9b0592e021dd0e9b9018defa324
martin Høva <mrhova46@gmail.com>
2024-03-26 13:58:31 +0100

    22 15 part2

--- 22/src/main/java/day15/main.py
+++ 22/src/main/java/day15/main.py
@@ -1,7 +1,5 @@
 import time
-from collections import Counter, defaultdict, deque
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 def dist(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
@@ -24,8 +22,7 @@ class Solution():
     def __init__(self, test=False):
         self.test = test
         self.filename = "testinput.txt" if self.test else "input.txt"
-        self.sensors = parse(self.filename)[0]
-        self.beacons = parse(self.filename)[1]
+        self.sensors, self.beacons = parse(self.filename)
         
     def part1(self):
         row = 10 if self.test else 2000000
@@ -49,23 +46,77 @@ class Solution():
         return len(overlaps) - counter
     
     def part2(self):
-        pass
-    
-    
-    def part2Old(self):
+        """The point we are looking for must be one square outside of the edge of one of the sensor-ranges.
+        We follow the edge of all the sensor ranges and check each point if it is the correct one.
+        """
         maxValue = 20 if self.test else 4000000
-        
-        for (x, y, d) in self.sensors:
-            x_s = [x, x + d, x, x - d, x]
-            y_s = [y - d, y, y + d, y, y - d]
-            plt.fill(x_s, y_s)
-        plt.plot(
-            [0, maxValue, maxValue, 0, 0],
-            [0, 0, maxValue, maxValue, 0],
-        )
-        plt.show()  
-        # plt.xlim(left=3403959, right=3403961)
-        # plt.ylim(top=3289728, bottom=3289730)
+        for i in tqdm(range(len(self.sensors))):
+            center_x, center_y, d = self.sensors[i]
+            #left to top
+            x = center_x - (d + 1)
+            y = center_y
+            while x != center_x or y != center_y - (d + 1):
+                if x < 0 or x > maxValue or y < 0 or y > maxValue:
+                    break
+                if self.checkPoint(x, y, i):
+                    print(x, y)
+                    return (x * 4000000) + y
+                x += 1
+                y -= 1
+            #top to right
+            x = center_x
+            y = center_y - (d + 1)
+            while x != center_x + (d + 1) or y != center_y:
+                if x < 0 or x > maxValue or y < 0 or y > maxValue:
+                    break
+                if self.checkPoint(x, y, i):
+                    print(x, y)
+                    return (x * 4000000) + y
+                x += 1
+                y += 1
+            #right to bottom
+            x = center_x + (d + 1)
+            y = center_y
+            while x != center_x or y != center_y + (d + 1):
+                if x < 0 or x > maxValue or y < 0 or y > maxValue:
+                    break
+                if self.checkPoint(x, y, i):
+                    print(x, y)
+                    return (x * 4000000) + y
+                x -= 1
+                y += 1
+            #bottom to left
+            x = center_x
+            y = center_y + (d + 1)
+            while x != center_x - (d + 1) or y != center_y:
+                if x < 0 or x > maxValue or y < 0 or y > maxValue:
+                    break
+                if self.checkPoint(x, y, i):
+                    print(x, y)
+                    return (x * 4000000) + y
+                x -= 1
+                y -= 1
+    
+    
+    def checkPoint(self, x: int, y: int, sensor_nr: int):
+        """A point is the correct point if it is outside the range of all other sensors.
+
+        Args:
+            x (int): x to check
+            y (int): y to check
+            sensor (int): index of sensor to not check
+
+        Returns:
+            boolean: True if this is the correct point
+        """
+        c = 0
+        for i in range(len(self.sensors)):
+            center_x, center_y, d = self.sensors[i]
+            if i == sensor_nr:
+                continue
+            if dist(x, y, center_x, center_y) > d:
+                c += 1
+        return c == len(self.sensors) - 1
     
     
 def main():
@@ -73,13 +124,13 @@ def main():
     
     s = Solution(test=True)
     print("---TEST---")
-    # print(f"part 1: {s.part1()}")
-    # print(f"part 2: {s.part2Old()}\n")
+    print(f"part 1: {s.part1()}")
+    print(f"part 2: {s.part2()}\n")
     
     s = Solution()
     print("---MAIN---")
     print(f"part 1: {s.part1()}")
-    print(f"part 2: {s.part2Old()}")
+    print(f"part 2: {s.part2()}")
     
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
     
