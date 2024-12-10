22110027d29e074e06db5c7eb5d57dd34db55c83
martin Høva <mrhova46@gmail.com>
2024-12-10 08:49:31 +0100

    24 10 cleanup

--- 24/10/main.py
+++ 24/10/main.py
@@ -13,16 +13,18 @@ class Solution():
         self.R = len(self.data)
         self.C = len(self.data[0])
         
-    def bfs(self, start_x, start_y):
+    def bfs(self, start_x, start_y, part: int):
         s = 0
         q = deque()
         q.append((start_x, start_y))
         visited = set()
         while q:
             x, y = q.popleft()
-            if (x, y) in visited:
-                continue
-            visited.add((x, y))
+            
+            if part == 1:
+                if (x, y) in visited:
+                    continue
+                visited.add((x, y))
             
             if self.data[y][x] == 9:
                 s += 1
@@ -33,56 +35,18 @@ class Solution():
                     continue
                 if self.data[ny][nx] != self.data[y][x] + 1:
                     continue
-                
                 q.append((nx, ny))
+                
         return s
+    
+    def run(self, part):
+        return sum(self.bfs(x, y, part) for x in range(self.C) for y in range(self.R) if self.data[y][x] == 0)
         
     def part1(self):
-        s = 0
-        R = len(self.data)
-        C = len(self.data[0])
-        
-        for y in range(R):
-            for x in range(C):
-                if self.data[y][x] != 0:
-                    continue
-                s += self.bfs(x, y)
-        return s
-    
-    def bfs2(self, start_x, start_y):
-        s = 0
-        q = deque()
-        q.append((start_x, start_y, -1, -1))
-        while q:
-            x, y, last_x, last_y = q.popleft()
-            
-            if self.data[y][x] == 9:
-                s += 1
-                continue
-            
-            for nx, ny in adjacent4(x, y):
-                if nx < 0 or nx >= self.C or ny < 0 or ny >= self.R:
-                    continue
-                if self.data[ny][nx] != self.data[y][x] + 1:
-                    continue
-                if (nx, ny) == (last_x, last_y):
-                    continue
-                
-                q.append((nx, ny, x, y))
-        return s
+        return self.run(1)
     
     def part2(self):
-        s = 0
-        R = len(self.data)
-        C = len(self.data[0])
-        
-        for y in range(R):
-            for x in range(C):
-                if self.data[y][x] != 0:
-                    continue
-                s += self.bfs2(x, y)
-        return s
-    
+        return self.run(2)
     
 def main():
     start = time.perf_counter()
